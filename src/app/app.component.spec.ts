36bc265462010c2c18a7c1b1599eae589f15108e
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-06 09:32:52 +0200

    Modified shell AppComponent and global style.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-frontend'`, () => {
+  it(`should have as title 'Topics and Summary'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('web-frontend');
+    expect(app.title).toEqual('Topics and Summary');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-frontend!');
+    expect(compiled.querySelector('h1').textContent).toContain('Topics and Summary');
   });
 });
