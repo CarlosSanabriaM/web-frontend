eceba8cc3f2c2d8f2c41cf24f83ccd6d298a3350
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-06 23:00:36 +0200

    Added HttpClientModule to AppModule import array.
    This makes HttpClient available everywhere in the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopicsSectionComponent } from './topics-section/topics-section.component';
 import { TextSectionComponent } from './text-section/text-section.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TextSectionComponent } from './text-section/text-section.component';
     TextSectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
