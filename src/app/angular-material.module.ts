433d6835bfe8ba19b0c305e8cc8c040527e9360b
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-08 00:10:46 +0200

    Added Angular Material Icons style and added tooltip to TopicsSectionComponent HTML.

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 /**
  * This module is used to import all the Angular Material components that will be used in the application.
  * Add the module in the imports and the exports arrays.
  */
 @NgModule({
-  imports: [ MatTableModule, MatSliderModule, MatButtonModule ],
-  exports: [ MatTableModule, MatSliderModule, MatButtonModule ]
+  imports: [ MatTableModule, MatSliderModule, MatButtonModule, MatTooltipModule ],
+  exports: [ MatTableModule, MatSliderModule, MatButtonModule, MatTooltipModule ]
 })
 export class AngularMaterialModule {}
