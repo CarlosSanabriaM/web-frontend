88a55369f616a02ee8fdbaadfcae47d9ee308491
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-08 19:57:23 +0200

    Implemented functionality in TextSectionComponent for Related Documents option.
    * Moved expansion-panel and tab-icon css to global css
    * Added divider to separate topics and text sections

--- src/app/text-section/text-section.component.ts
+++ src/app/text-section/text-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TextService } from '../text.service';
 import { TextTopicProb } from '../dtos/text-topic-prob';
+import { TextRelatedDoc } from '../dtos/text-related-doc';
 
 @Component({
   selector: 'app-text-section',
@@ -10,11 +11,17 @@ import { TextTopicProb } from '../dtos/text-topic-prob';
 export class TextSectionComponent implements OnInit {
 
   sectionName = 'Texto';
+  relatedTopics: TextTopicProb[];
+  relatedDocuments: TextRelatedDoc[];
   // TODO: This values shouldn't be hardcoded here
   textAreaNumRows = 30;
   initialMaxNumTopics = 6; // initial value for the max num topics slider
-  maxNumTopics = 17; // max num topics for the max num topics slider
-  relatedTopics: TextTopicProb[];
+  maxNumTopics = 17;
+  initialNumDocuments = 2;
+  maxNumDocuments = 10;
+  relatedDocumentSummaryMaxLength = 150;
+
+  // max num topics for the max num topics slider
 
   constructor(private textService: TextService) { }
 
@@ -29,6 +36,16 @@ export class TextSectionComponent implements OnInit {
 
     this.textService.getRelatedTopics(text, maxNumTopics)
       .subscribe(relatedTopics => this.relatedTopics = relatedTopics);
+  }
+
+  getRelatedDocuments(text: string, numDocuments: number) {
+    if (text.length === 0) {
+      console.log('Text is empty');
+      return;
+    }
+
+    this.textService.getRelatedDocuments(text, numDocuments)
+      .subscribe(relatedDocuments => this.relatedDocuments = relatedDocuments);
   }
 
 }
