a26aaf529d38e40d09a97db175c4bccb9be7dce1
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-08 14:03:16 +0200

    Created a tab panel with 2 tabs: topics in text and wordcloud formats.
    * Imported MatTabsModule and MatIconModule modules

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -5,13 +5,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 /**
  * This module is used to import all the Angular Material components that will be used in the application.
  * Add the module in the imports and the exports arrays.
  */
 @NgModule({
-  imports: [ MatTableModule, MatSliderModule, MatButtonModule, MatTooltipModule, MatExpansionModule, MatCardModule ],
-  exports: [ MatTableModule, MatSliderModule, MatButtonModule, MatTooltipModule, MatExpansionModule, MatCardModule ]
+  imports: [
+    MatTableModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule
+  ],
+  exports: [
+    MatTableModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule
+  ]
 })
 export class AngularMaterialModule {}
