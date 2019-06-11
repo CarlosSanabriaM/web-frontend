9912f23cc7787ef061f213881a33ce4909e30b77
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-11 21:57:49 +0200

    Added footer, modified page global container, and added components for header and footer.
    * Options in the text section have a margin of 3% in the bottom to avoid
      collisions with other components in small screens.
    * Removed @import of bootstrap-grid.min.css because Bootstrap grid is included in bootstrap.min.css.
    * Replaced css "body margin: 2em" by "#main-container width: 96%"
    * Container fluid doesn't wrap header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TopicsSectionComponent } from './topics-section/topics-section.component';
-import { TextSectionComponent } from './text-section/text-section.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { AngularBootstrapModule } from './angular-bootstrap.module';
+import { TopicsSectionComponent } from './topics-section/topics-section.component';
+import { TextSectionComponent } from './text-section/text-section.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopicsSectionComponent,
-    TextSectionComponent
+    TextSectionComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
