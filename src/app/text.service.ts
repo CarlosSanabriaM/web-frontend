9e967126b602ba0df47aa32b7f28b3fd2e4c84b0
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-14 20:54:16 +0200

    Refactored handleError() function to UtilsService.

--- src/app/text.service.ts
+++ src/app/text.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { TextTopicProb } from './dtos/text-topic-prob';
 import { TextRelatedDoc } from './dtos/text-related-doc';
 import { TextSummary } from './dtos/text-summary';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,8 @@ export class TextService {
 
   private apiTextUrl = `${environment.baseUrl}/user/api/text`;  // URL to topics and summary API text 'section'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private utilsService: UtilsService) { }
 
   /**
    * POST the given text to the REST API and return the text-topics probabilities.
@@ -32,7 +34,7 @@ export class TextService {
     return this.http.post<TextTopicProb[]>(url, formData).pipe(
       // TODO: Modify tap?
       tap(_ => console.log(`fetched related topics with max_num_topics=${maxNumTopics}`)),
-      catchError(this.handleError)
+      catchError(this.utilsService.handleError)
     );
   }
 
@@ -51,7 +53,7 @@ export class TextService {
     return this.http.post<TextRelatedDoc[]>(url, formData).pipe(
       // TODO: Modify tap?
       tap(_ => console.log(`fetched related documents with num_documents=${numDocuments}`)),
-      catchError(this.handleError)
+      catchError(this.utilsService.handleError)
     );
   }
 
@@ -70,29 +72,8 @@ export class TextService {
     return this.http.post<TextSummary>(url, formData).pipe(
       // TODO: Modify tap?
       tap(_ => console.log(`fetched text summary with num_summary_sentences=${numSentences}`)),
-      catchError(this.handleError)
+      catchError(this.utilsService.handleError)
     );
-  }
-
-  // TODO: Revise this method
-  // TODO: Don't repeat the code of this method in the 2 services. Move to a common place.
-  /**
-   * Method for handling http errors.
-   */
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
   }
 
 }
--- src/app/topics.service.ts
+++ src/app/topics.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Topic } from './dtos/topic';
 import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { TopicImageUrl } from './dtos/topic-image-url';
 import { ReprDocOfTopic } from './dtos/repr-doc-of-topic';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,8 @@ export class TopicsService {
 
   private apiTopicsUrl = `${environment.baseUrl}/user/api/topics`;  // URL to topics and summary API topics 'section'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private utilsService: UtilsService) { }
 
   /**
    * GET topics in text format from the REST API.
@@ -26,7 +28,7 @@ export class TopicsService {
     return this.http.get<Topic[]>(url).pipe(
       // TODO: Modify tap?
       tap(_ => console.log(`fetched topics in text format with num_keywords=${numKeywords}`)),
-      catchError(this.handleError)
+      catchError(this.utilsService.handleError)
     );
   }
 
@@ -45,7 +47,7 @@ export class TopicsService {
         topic: topicImageUrl.topic,
         image_url: `${environment.baseUrl}${topicImageUrl.image_url}`
       }))),
-      catchError(this.handleError)
+      catchError(this.utilsService.handleError)
     );
   }
 
@@ -60,29 +62,8 @@ export class TopicsService {
     return this.http.get<ReprDocOfTopic[]>(url).pipe(
       // TODO: Modify tap?
       tap(_ => console.log(`fetched the ${numDocuments} most representative documents of topic ${topicId}`)),
-      catchError(this.handleError)
+      catchError(this.utilsService.handleError)
     );
-  }
-
-  // TODO: Revise this method
-  // TODO: Don't repeat the code of this method in the 2 services. Move to a common place.
-  /**
-   * Method for handling http errors.
-   */
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
   }
 
 }
