4f310cb99ca9b3213faeb3a33b11c4b407c768d6
CarlosSanabriaM <uo250707@uniovi.es>
2019-06-21 18:18:47 +0200

    Removed TODOs about i18n of strings in the Typescript, because it isn't possible with Angular.

--- src/app/utils.service.ts
+++ src/app/utils.service.ts
@@ -37,30 +37,28 @@ export class UtilsService {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
+
       // Return an observable with a user-facing error message
-      // TODO: English version: return throwError('A client-side or network error has occurred. Please, try again later.');
       return throwError('Ha ocurrido un error causado por el frontend o por la red. Por favor, inténtalo más tarde.');
+
     } else {
       // The backend returned an unsuccessful response code.
       console.error(`Backend returned code ${error.status}, with the following message: '${error.message}'.`);
+
       // Return an observable with a user-facing error message, based on the error status code
-      // TODO: English version: const wrapMessageFunc = (inner: string) => `The backend returned an unsuccessful response code. ${inner} Please, try again later.`;
-      const wrapMessageFunc = (inner: string) => `El backend ha devuelto un código de respuesta erróneo. ${inner} Por favor, inténtalo más tarde.`;
+      const wrapMessageFunc =
+        (inner: string) => `El backend ha devuelto un código de respuesta erróneo. ${inner} Por favor, inténtalo más tarde.`;
+
       switch (error.status) {
         case 400:
-          // TODO: English version: return throwError(wrapMessageFunc('Bad request sent to the server.'));
           return throwError(wrapMessageFunc('Petición con sintáxis inválida enviada al servidor.'));
         case 404:
-          // TODO: English version: return throwError(wrapMessageFunc('Value asked to the server not found.'));
           return throwError(wrapMessageFunc('El valor solicitado al servidor no se ha encontrado.'));
         case 422:
-          // TODO: English version: return throwError(wrapMessageFunc('Optional values sent to the server were wrong.'));
           return throwError(wrapMessageFunc('Los valores opcionales enviados al servidor eran erróneos.'));
         case 500:
-          // TODO: English version: return throwError(wrapMessageFunc('Server error.'));
           return throwError(wrapMessageFunc('Error del servidor.'));
         default:
-          // TODO: English version: return throwError(wrapMessageFunc(''));
           return throwError(wrapMessageFunc(''));
       }
     }
